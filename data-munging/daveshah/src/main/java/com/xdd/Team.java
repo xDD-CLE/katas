3cf76430d1f196aeb2ecba61e835125338ab17be
Dave Shah <dave.shah@genomoncology.com>
2015-08-18 22:05:09 -0400

    Remove magic numbers

--- data-munging/daveshah/src/main/java/com/xdd/Team.java
+++ data-munging/daveshah/src/main/java/com/xdd/Team.java
@@ -1,11 +1,16 @@
 package com.xdd;
 
 
+import static java.lang.Integer.parseInt;
+
 /**
  * Created by shah on 8/17/15.
  */
 public class Team {
     public static final int NUMBER_OF_COLUMNS = 9;
+    public static final int POSITION_OF_NAME = 1;
+    public static final int POSITION_OF_GOALS_FOR = 6;
+    public static final int POSITION_OF_GOALS_AGAINST = 7;
     private final String name;
     private final boolean valid;
     private final int differenceInGoals;
@@ -14,8 +19,8 @@ public class Team {
         if (input != null) {
             String[] split = input.replaceFirst("\\W+", "").split("\\W+");
             this.valid = split.length == NUMBER_OF_COLUMNS;
-            this.name = valid ? split[1] : "";
-            this.differenceInGoals = valid ? Integer.parseInt(split[6]) - Integer.parseInt(split[7]) : 0;
+            this.name = valid ? split[POSITION_OF_NAME] : "";
+            this.differenceInGoals = valid ? parseInt(split[POSITION_OF_GOALS_FOR]) - parseInt(split[POSITION_OF_GOALS_AGAINST]) : 0;
         } else {
             this.name = "";
             this.valid = false;
