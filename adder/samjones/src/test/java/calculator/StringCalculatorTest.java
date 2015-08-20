4f606b1c49c97b0e6959808a00a1a6b9a3ecbf75
Sam Jones <sam@samljones.com>
2015-08-19 21:56:00 -0400

    [samjones] [adder] Step 2 - Simple String Calculator

--- adder/samjones/src/test/java/calculator/StringCalculatorTest.java
+++ adder/samjones/src/test/java/calculator/StringCalculatorTest.java
@@ -9,6 +9,7 @@ public class StringCalculatorTest {
     @Test
     public void stepOne() throws Exception {
         StringCalculator calculator = new StringCalculator();
+
         assertEquals(0, calculator.add(null));
         assertEquals(0, calculator.add(""));
 
@@ -18,5 +19,22 @@ public class StringCalculatorTest {
         assertEquals(2, calculator.add("1,1"));
         assertEquals(5, calculator.add("2,3"));
         assertEquals(6, calculator.add("1,2,3"));
+    }
+
+    @Test
+    public void step2() throws Exception {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(10, calculator.add("1,2,3,4"));
+
+        StringBuilder stringValue = new StringBuilder();
+        for (int i = 0; i < 500; i++) {
+            if (stringValue.length() > 0) {
+                stringValue.append(",");
+            }
+            stringValue.append(i);
+        }
+        assertEquals(124750, calculator.add(stringValue.toString()));
+
     }
 }
