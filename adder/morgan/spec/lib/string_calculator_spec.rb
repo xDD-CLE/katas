af3260b3ec8301cc52b581177a24d16fee648207
DXM742 <doug.morgan@leandogsoftware.com>
2015-08-21 15:10:53 -0400

    First Failing Test

--- adder/morgan/spec/lib/string_calculator_spec.rb
+++ adder/morgan/spec/lib/string_calculator_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe 'string calculator' do
   describe 'addition' do
-    it 'is true' do
-      expect(true).to eq true
-    end
+      describe 'adder' do
+        it 'should return 0 from empty string' do
+          expect(Adder.add("")).to eq 0
+        end
+      end
   end
 end
