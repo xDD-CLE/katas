bfb379955f0b00bb8f358e9c40c97de097e78618
DXM742 <doug.morgan@leandogsoftware.com>
2015-08-21 15:16:04 -0400

    Failing: should return 1 from "1"

--- adder/morgan/spec/lib/string_calculator_spec.rb
+++ adder/morgan/spec/lib/string_calculator_spec.rb
@@ -6,6 +6,10 @@ describe 'string calculator' do
         it 'should return 0 from empty string' do
           expect(Adder.add("")).to eq 0
         end
+
+        it 'should return 1 from "1"' do
+          expect(Adder.add("1")).to eq 1
+        end
       end
   end
 end
