df73bd76ca18995885a08fd71cb070ab13c2893b
DXM742 <doug.morgan@leandogsoftware.com>
2015-08-21 15:29:20 -0400

    Added context for clarity

--- adder/morgan/spec/lib/string_calculator_spec.rb
+++ adder/morgan/spec/lib/string_calculator_spec.rb
@@ -7,9 +7,14 @@ describe 'string calculator' do
           expect(Adder.add("")).to eq 0
         end
 
-        [{input: "1", expected: 1}, {input: "5", expected: 5}].each do |test_case|
-          it "should return #{test_case[:input]} from \"1\"" do
-            expect(Adder.add(test_case[:input])).to eq test_case[:expected]
+        context 'should handle single addends' do
+          [
+            {input: "1", expected: 1},
+            {input: "5", expected: 5}
+          ].each do |test_case|
+            it "should return #{test_case[:input]} from \"1\"" do
+              expect(Adder.add(test_case[:input])).to eq test_case[:expected]
+            end
           end
         end
       end
