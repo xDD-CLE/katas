9256cb95b7cc83013833d52e81687fa5c5b9a218
DXM742 <doug.morgan@leandogsoftware.com>
2015-08-21 16:20:43 -0400

    Better But Not Loving It

--- adder/morgan/lib/adder.rb
+++ adder/morgan/lib/adder.rb
@@ -1,18 +1,22 @@
 class Adder
 
   ADDITION = lambda {|addend,sum| sum += addend }
-  DELIMITERS = /[,\n]/
 
   def self.add(string_of_addends)
     return 0 if string_of_addends.empty?
+    addends = normalize(string_of_addends)
+    addends.map(&:to_i).inject(0, &ADDITION)
+  end
+
+  def self.normalize(string_of_addends)
+    delimiters = /[,\n]/
 
     if string_of_addends[0..1] == "//"
       delimiter_override = string_of_addends[2]
       string_of_addends.slice!(0..2)
       string_of_addends.tr!(delimiter_override, ',')
     end
-    
-    addends = string_of_addends.split(DELIMITERS)
-    addends.map(&:to_i).inject(0, &ADDITION)
+    string_of_addends.split(delimiters)
   end
+
 end
