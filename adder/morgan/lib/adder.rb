e4988c3eff8adab06ea6cd27ca32639e974adfd7
DXM742 <doug.morgan@leandogsoftware.com>
2015-08-21 16:10:50 -0400

    Green. But needs work

--- adder/morgan/lib/adder.rb
+++ adder/morgan/lib/adder.rb
@@ -5,6 +5,13 @@ class Adder
 
   def self.add(string_of_addends)
     return 0 if string_of_addends.empty?
+
+    if string_of_addends[0..1] == "//"
+      delimiter_override = string_of_addends[2]
+      string_of_addends.slice!(0..2)
+      string_of_addends.tr!(delimiter_override, ',')
+    end
+    
     addends = string_of_addends.split(DELIMITERS)
     addends.map(&:to_i).inject(0, &ADDITION)
   end
