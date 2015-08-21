97125d5e171b5e01758afa01c7752d6cd0af2def
DXM742 <doug.morgan@leandogsoftware.com>
2015-08-21 17:19:39 -0400

    Playing Around with naming a bit

--- adder/morgan/lib/adder.rb
+++ adder/morgan/lib/adder.rb
@@ -1,34 +1,42 @@
 class Adder
 
-  ADDITION = lambda {|addend,sum| sum += addend }
-  NEGATIVE_NUMBER = lambda {|num| num < 0}
-
   def add(string_of_addends)
     return 0 if string_of_addends.empty?
-    addends = normalize(string_of_addends)
-    do_not_allow_negative_numbers(addends)
-    addends.inject(0, &ADDITION)
+    addends = StringNormalizer.to_array_of_integers(string_of_addends)
+    raise_error_if_negative_numbers_included_in addends
+    sum_up addends
   end
 
   private
 
-  def normalize(addends)
-    delimiters = /[,\n]/
-    if addends[0..1] == "//"
-      delimiter_override = addends[2]
-      addends.slice!(0..2)
-      addends.tr!(delimiter_override, ',')
-    end
-    addends.split(delimiters).map(&:to_i)
+  def raise_error_if_negative_numbers_included_in(addends)
+    negative_numbers = lambda {|num| num < 0}
+    negative_addends = addends.select(&negative_numbers)
+    raise ArgumentError, negative_number_message_with(negative_addends) if negative_addends.any?
   end
 
-  def do_not_allow_negative_numbers(addends)
-    negative_addends = addends.select(&NEGATIVE_NUMBER)
-    raise ArgumentError, negative_number_message(negative_addends) if negative_addends.any?
-  end
-
-  def negative_number_message(negative_addends)
+  def negative_number_message_with(negative_addends)
     "Negatives not allowed: [#{negative_addends.join(',')}]"
+  end
+
+  def sum_up(addends)
+    addition = lambda {|addend,sum| sum += addend }
+    addends.inject(0, &addition)
+  end
+end
+
+class StringNormalizer
+
+  DELIMITERS = /[,\n]/
+
+  def self.to_array_of_integers(numbers)
+    if numbers[0..1] == "//"
+      delimiter_override = numbers[2]
+      numbers.slice!(0..2)
+      numbers.tr!(delimiter_override, ',')
+    end
+
+    numbers.split(DELIMITERS).map(&:to_i)
   end
 
 end
