92ee6fc4fa7e6f787029649d2beb703eaee346b7
Jake Gordon <jacob.gordon@Emerson.com>
2015-08-21 22:58:43 -0400

    saving files before refreshing line endings.

---
+++ data-munging/jakejgordon/katas/Kata04.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace katas
+{
+    public class Kata04
+    {
+        const int COLUMN_INDEX_DAY_NUMBER = 0;
+        const int COLUMN_INDEX_MAXIMUM_TEMPERATURE = 1;
+        const int COLUMN_INDEX_MINIMUM_TEMPERATURE = 2;
+
+        public void FindSmallestTemperatureSpread()
+        {
+            var fileContents = File.ReadAllText(@"Data\weather.dat");
+
+            var tableRows = GetTableRows(fileContents);
+            RemoveAnyRowsThatArentForADayOfTheMonth(tableRows);
+            var table = GetTableWithRowsAndColumns(tableRows);
+            var temperatureSpreads = this.GetTemperatureSpreads(table);
+            var dayWithLowestTemperatureSpread = this.GetDayWithLowestTemperatureSpread(temperatureSpreads);
+            Debug.WriteLine(string.Format("The day with the lowest temperature spread is day {0}", dayWithLowestTemperatureSpread));
+        }
+
+        private static void RemoveAnyRowsThatArentForADayOfTheMonth(List<string> tableRows)
+        {
+            var rowsToRemove = new List<string>();
+            foreach (var row in tableRows)
+            {
+                decimal placeHolder;
+                if (row.Length < 4 || !decimal.TryParse(row.Substring(0, 4), out placeHolder))
+                {
+                    rowsToRemove.Add(row);
+                }
+            }
+
+            rowsToRemove.ForEach(rowToRemove => tableRows.Remove(rowToRemove));
+        }
+
+        public virtual List<string> GetTableRows(string entireFile)
+        {
+            return Regex.Split(entireFile, "\n").ToList();
+        }
+
+        public virtual List<List<decimal>> GetTableWithRowsAndColumns(List<string> unparsedRows)
+        {
+            const string REGEX_ANYTHING_BUT_NUMBERS_AND_PERIOD = @"[^0-9\.]+";
+            return unparsedRows.Select(row => Regex.Split(row.Trim(), REGEX_ANYTHING_BUT_NUMBERS_AND_PERIOD).ToList())
+                .Select(singleRowColumns => singleRowColumns.Select(decimal.Parse).ToList())
+                    .ToList();
+        }
+
+        public Dictionary<int, decimal> GetTemperatureSpreads(List<List<decimal>> table)
+        {
+            return table.Select(row => new
+            {
+                DayNumber = row[COLUMN_INDEX_DAY_NUMBER],
+                TemperatureSpread = (row[COLUMN_INDEX_MAXIMUM_TEMPERATURE] - row[COLUMN_INDEX_MINIMUM_TEMPERATURE])
+            }).ToDictionary(anonymousResult => (int)anonymousResult.DayNumber, anonymousResult => anonymousResult.TemperatureSpread);
+        }
+
+        private int GetDayWithLowestTemperatureSpread(Dictionary<int, decimal> dayToTemperatureSpread)
+        {
+            decimal minValue = dayToTemperatureSpread.Min(entry => entry.Value);
+            return dayToTemperatureSpread.First(entry => entry.Value == minValue).Key;
+        }
+    }
+}
---
+++ data-munging/jakejgordon/katas/Kata04Tests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace katas
+{
+	[TestFixture]
+    public class Kata04Tests
+	{
+	    private Kata04 kata04;
+
+	    [SetUp]
+	    public void SetUp()
+	    {
+	        kata04 = new Kata04();
+	    }
+
+	    [Test]
+	    public void DoIt()
+	    {
+			kata04.FindSmallestTemperatureSpread();
+	    }
+
+	    [Test]
+	    public void ItGetsRowsBasedOnTheNewLineCharacter()
+	    {
+            const string FOUR_ROWS = @"  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP
+
+   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5
+   2  79    63    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5";
+
+	        var results = kata04.GetTableRows(FOUR_ROWS);
+
+            Assert.That(results.Count, Is.EqualTo(4));
+	    }
+
+	    [Test]
+	    public void ItGetsColumnsBasedOnWhiteSpace()
+	    {
+	        const string SINGLE_ROW = @"  1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5";
+	        var tableWithSingleRow = new List<string>
+	        {
+	            SINGLE_ROW
+	        };
+            
+            var results = kata04.GetTableWithRowsAndColumns(tableWithSingleRow);
+
+            Assert.That(results.Count, Is.EqualTo(1));
+            Assert.That(results[0].Count, Is.EqualTo(14));
+            Assert.That(results[0][13], Is.EqualTo(1004.5));
+	    }
+
+	    [Test]
+	    public void ItGetsTheTemperatureSpreads()
+	    {
+	        const int DAY_NUMBER_ONE = 1;
+	        const int DAY_NUMBER_TWO = 2;
+	        var table = new List<List<decimal>>
+	        {
+	            new List<decimal>
+	            {
+	                DAY_NUMBER_ONE,
+	                100,
+	                0
+	            },
+	            new List<decimal>
+	            {
+	                DAY_NUMBER_TWO,
+	                100,
+	                99
+	            }
+	        };
+
+	        var results = kata04.GetTemperatureSpreads(table);
+
+            Assert.That(results[DAY_NUMBER_ONE], Is.EqualTo(100));
+            Assert.That(results[DAY_NUMBER_TWO], Is.EqualTo(1));
+	    }
+    }
+}
