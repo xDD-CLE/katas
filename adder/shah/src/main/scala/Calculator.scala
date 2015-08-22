80481a1d0ecee2cbf31e3fdb6b2f7ed5ccbd066a
Dave Shah <dave.shah@genomoncology.com>
2015-08-22 12:18:58 -0400

    It supports different delimiters

--- adder/shah/src/main/scala/Calculator.scala
+++ adder/shah/src/main/scala/Calculator.scala
@@ -4,7 +4,30 @@
 case class Calculator(numbers :String) {
 
   private val DELIMITER = ","
+  private val NEW_LINE = "\n"
+  private val CUSTOM_DELIMITER_MARKER = "//"
 
-  def add : Int = if (numbers == "") 0 else numbers.replace("\n",DELIMITER).split(DELIMITER).map(_.toInt).sum
+  def add : Int = {
+    if (numbers == "") {
+      0
+    } else {
+      changeToDelimiter(numbers)
+        .replace(NEW_LINE,DELIMITER)
+        .split(DELIMITER)
+        .filterNot(_.isEmpty)
+        .map(_.toInt)
+        .sum
+    }
+  }
 
+  def changeToDelimiter(input :String) : String = {
+    val potentialDelimiter = input.stripPrefix(CUSTOM_DELIMITER_MARKER).charAt(0)
+    if(potentialDelimiter.isDigit){
+      input
+    } else {
+      input
+        .stripPrefix(CUSTOM_DELIMITER_MARKER + potentialDelimiter)
+        .replaceAll(potentialDelimiter.toString,DELIMITER)
+    }
+  }
 }
