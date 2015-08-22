05b51a42c5f0545a16f4830255185b108658f706
Dave Shah <dave.shah@genomoncology.com>
2015-08-22 12:54:47 -0400

    It ignores numbers > 1000

--- adder/shah/src/test/scala/StringCalculatorSpec.scala
+++ adder/shah/src/test/scala/StringCalculatorSpec.scala
@@ -7,48 +7,38 @@ import org.scalatest.Matchers._
 @RunWith(classOf[JUnitRunner])
 class StringCalculatorSpec extends FlatSpec {
 
-  "A string calculator" should "return 0 for an empty string" in {
-    val result = Calculator("").add
-    result should be (0)
+  it should "return 0 for an empty string" in {
+    Calculator("").add should be(0)
   }
 
-  "A string calculator" should "return the number when adding only the number" in {
-    val result = Calculator("1").add
-
-    result should be (1)
+  it should "return the number when adding only the number" in {
+    Calculator("1").add should be(1)
   }
 
-  "A string calculator" should "return the sum of two comma separated numbers" in {
-    val result = Calculator("1,2").add
-
-    result should be (3)
+  it should "return the sum of two comma separated numbers" in {
+    Calculator("1,2").add should be(3)
   }
 
-  "A string calculator" should "return the sum of many comma separated numbers" in {
-    val result = Calculator("1,2,3,7").add
-
-    result should be (13)
+  it should "return the sum of many comma separated numbers" in {
+    Calculator("1,2,3,7").add should be(13)
   }
 
-  "A string calculator" should "Allow the Add method to handle new lines between numbers (instead of commas)" in {
-    val result = Calculator("1\n2,3").add
-
-    result should be (6)
+  it should "Allow the Add method to handle new lines between numbers (instead of commas)" in {
+    Calculator("1\n2,3").add should be(6)
   }
 
-  "A string calculator" should "Support different delimiters for the addition of numbers" in {
-    val result = Calculator("//;\n1;2").add
-
-    result should be (3)
+  it should "Support different delimiters for the addition of numbers" in {
+    Calculator("//;\n1;2").add should be(3)
   }
 
-  "A string calculator" should "not support the addition of negative numbers" in {
-    the [Exception] thrownBy {
+  it should "not support the addition of negative numbers" in {
+    the[Exception] thrownBy {
       Calculator("//;\n-1;-2").add
     } should have message "Negatives not allowed: -1, -2"
+  }
 
-
-
+  it should "ignore numbers bigger than 1000" in {
+    Calculator("1001,2").add should be(2)
   }
 
 }
