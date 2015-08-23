808019def690135d28b459517d2d480976ba9c8f
Dave Shah <dave.shah@genomoncology.com>
2015-08-22 23:05:23 -0400

    Rename

--- adder/shah/src/test/scala/StringCalculatorSpec.scala
+++ adder/shah/src/test/scala/StringCalculatorSpec.scala
@@ -8,45 +8,45 @@ import org.scalatest.Matchers._
 class StringCalculatorSpec extends FlatSpec {
 
   it should "return 0 for an empty string" in {
-    Calculator("").add should be(0)
+    StringCalculator("").add should be(0)
   }
 
   it should "return the number when adding only the number" in {
-    Calculator("1").add should be(1)
+    StringCalculator("1").add should be(1)
   }
 
   it should "return the sum of two comma separated numbers" in {
-    Calculator("1,2").add should be(3)
+    StringCalculator("1,2").add should be(3)
   }
 
   it should "return the sum of many comma separated numbers" in {
-    Calculator("1,2,3,7").add should be(13)
+    StringCalculator("1,2,3,7").add should be(13)
   }
 
   it should "Allow the Add method to handle new lines between numbers (instead of commas)" in {
-    Calculator("1\n2,3").add should be(6)
+    StringCalculator("1\n2,3").add should be(6)
   }
 
   it should "Support different delimiters for the addition of numbers" in {
-    Calculator("//;\n1;2").add should be(3)
+    StringCalculator("//;\n1;2").add should be(3)
   }
 
   it should "not support the addition of negative numbers" in {
     the[Exception] thrownBy {
-      Calculator("//;\n-1;-2").add
+      StringCalculator("//;\n-1;-2").add
     } should have message "Negatives not allowed: -1,-2"
 
     the[Exception] thrownBy {
-      Calculator("//;\n-3;-2").add
+      StringCalculator("//;\n-3;-2").add
     } should have message "Negatives not allowed: -3,-2"
   }
 
   it should "ignore numbers bigger than 1000" in {
-    Calculator("1001,2").add should be(2)
+    StringCalculator("1001,2").add should be(2)
   }
 
   it should "Allow delimeters of any length" in {
-    Calculator("//[***]\n1***2***3").add should be(6)
+    StringCalculator("//[***]\n1***2***3").add should be(6)
   }
 
 }
