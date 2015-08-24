c18651930b86cc740aa68823f055c72e5c8066e6
Jake Gordon <jacob.gordon@Emerson.com>
2015-08-23 21:59:12 -0400

    returns 0 for empty string

---
+++ data-munging/jakejgordon/katas/AdderKata/Adder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace katas.AdderKata
+{
+    public class Adder
+    {
+        public int Add(string commaSeparatedNumbers)
+        {
+            return 0;
+        }
+    }
+}
---
+++ data-munging/jakejgordon/katas/AdderKata/AdderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace katas.AdderKata
+{
+    [TestFixture]
+    public class AdderTests
+    {
+        private Adder adder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            adder = new Adder();
+        }
+
+        [Test]
+        public void ItReturnsZeroIfThereIsOnlyAnEmptyString()
+        {
+            var result = adder.Add(string.Empty);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+    }
+}
