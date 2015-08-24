0d3f843735f1b272f8bff0b410be539bd9d90ed7
davidhatten <david.r.hatten@gmail.com>
2015-08-24 09:41:34 -0400

    added second test, doesn't pass yet

--- adder/davidhatten/src/test/java/StringCalculatorTest.java
+++ adder/davidhatten/src/test/java/StringCalculatorTest.java
@@ -1,15 +1,28 @@
 import kata.adder.Calculator;
 import kata.adder.StringCalculator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
+
     @Test
     public void shouldAddNoNumbers() {
-        Calculator calculator = new StringCalculator();
         int result = calculator.add("");
         Assert.assertEquals("Adding an empty string should result in 0", 0, result);
+    }
+
+    @Test
+    public void shouldAddOneNumber() {
+        int result = calculator.add("1");
+        Assert.assertEquals("One added with nothing should be 1", 1, result);
     }
 
 }
