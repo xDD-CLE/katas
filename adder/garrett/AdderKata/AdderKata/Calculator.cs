8014a5d25289cc6c79cd4af7d5952426f3125fa1
gopenshaw <garrett.j.openshaw@gmail.com>
2015-08-28 08:14:04 -0400

    Extracted methods

--- adder/garrett/AdderKata/AdderKata/Calculator.cs
+++ adder/garrett/AdderKata/AdderKata/Calculator.cs
@@ -7,21 +7,43 @@ namespace AdderKata
 {
     public class Calculator
     {
-        public int Add(string values)
+        public int Add(string input)
         {
-            if (string.IsNullOrEmpty(values))
+            if (string.IsNullOrEmpty(input))
             {
                 return 0;
             }
 
+            var delimiters = GetDelimiters(input);
+            var numbers = GetNumbers(input, delimiters);
+            VerifyNoNegativeNumbers(numbers);
+
+            return numbers.Sum(x => x);
+        }
+
+        private static List<char> GetDelimiters(string values)
+        {
             var delimiters = new List<char> {',', '\n'};
             if (values.StartsWith("//"))
             {
                 delimiters.Add(Convert.ToChar(values.Substring(2, 1)));
+            }
+
+            return delimiters;
+        }
+
+        private static List<int> GetNumbers(string values, List<char> delimiters)
+        {
+            if (values.StartsWith("//"))
+            {
                 values = values.Substring(4);
             }
 
-            var numbers = values.Split(delimiters.ToArray()).Select(s => Convert.ToInt16(s)).ToList();
+            return values.Split(delimiters.ToArray()).Select(s => Convert.ToInt32(s)).ToList();
+        }
+
+        private static void VerifyNoNegativeNumbers(List<int> numbers)
+        {
             var negativeNumbers = numbers.Where(x => x < 0).ToList();
             if (negativeNumbers.Count > 0)
             {
@@ -29,8 +51,6 @@ namespace AdderKata
                 negativeNumbers.ForEach(x => message += " " + x);
                 throw new ArgumentException(message);
             }
-
-            return numbers.Sum(x => x);
         }
     }
 
