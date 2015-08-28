6047d7088eccb3b9b9dc027c38f7acfa697be6b7
gopenshaw <garrett.j.openshaw@gmail.com>
2015-08-28 08:09:15 -0400

    Functionality for the first five steps.

---
+++ adder/garrett/AdderKata/AdderKata/Calculator.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AdderKata
+{
+    public class Calculator
+    {
+        public int Add(string values)
+        {
+            if (string.IsNullOrEmpty(values))
+            {
+                return 0;
+            }
+
+            var delimiters = new List<char> {',', '\n'};
+            if (values.StartsWith("//"))
+            {
+                delimiters.Add(Convert.ToChar(values.Substring(2, 1)));
+                values = values.Substring(4);
+            }
+
+            var numbers = values.Split(delimiters.ToArray()).Select(s => Convert.ToInt16(s)).ToList();
+            var negativeNumbers = numbers.Where(x => x < 0).ToList();
+            if (negativeNumbers.Count > 0)
+            {
+                var message = "negatives not allowed";
+                negativeNumbers.ForEach(x => message += " " + x);
+                throw new ArgumentException(message);
+            }
+
+            return numbers.Sum(x => x);
+        }
+    }
+
+    class CalculatorTests
+    {
+        [Test]
+        public void It_Returns_Zero_For_Empty_String()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+
+            //--Act
+            int result = calculator.Add(string.Empty);
+
+            //--Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void It_Returns_The_Number()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+            var value = "5";
+
+            //--Act
+            int result = calculator.Add(value);
+
+            //--Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void It_Returns_A_Sum()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+            var values = "5,27";
+
+            //--Act
+            int result = calculator.Add(values);
+
+            //--Assert
+            Assert.AreEqual(32, result);
+        }
+
+        [Test]
+        public void It_Can_Parse_Numbers_On_Different_Lines()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+            var values = "5,2\n8";
+
+            //--Act
+            int result = calculator.Add(values);
+
+            //--Assert
+            Assert.AreEqual(15, result);
+        }
+
+        [Test]
+        public void It_Supports_Custom_Delimiters()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+            var input = "//;\n1;5;8";
+
+            //--Act
+            int result = calculator.Add(input);
+
+            //--Assert
+            Assert.AreEqual(14, result);
+        }
+
+        [Test]
+        public void It_Does_Not_Accept_Negative_Numbers()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+            var input = "8,-5";
+
+            //--Act/Assert
+            Assert.Throws<ArgumentException>(() => calculator.Add(input));
+        }
+
+        [Test]
+        public void It_Says_Which_Inputs_Were_Invalid()
+        {
+            //--Arrange
+            var calculator = new Calculator();
+            var input = "-6,-15";
+
+            //--Act/Assert
+            Exception ex = Assert.Throws<ArgumentException>(() => calculator.Add(input));
+            Assert.AreEqual("negatives not allowed -6 -15", ex.Message);
+        }
+    }
+}
