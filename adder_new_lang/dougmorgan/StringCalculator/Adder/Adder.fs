52bef488515456d39a2f11d45a50d47ab79ea07a
Doug Morgan <doug.morgan@leandogsoftware.com>
2015-08-29 08:51:25 -0400

    F# String Calculator (Still a few more cases to add)

---
+++ adder_new_lang/dougmorgan/StringCalculator/Adder/Adder.fs
@@ -0,0 +1,27 @@
+﻿namespace StringCalculator
+    
+    module Adder = 
+        open Parser
+        open System
+
+        let private negativeNumberExceptionMessage = fun(negativeAddends:int list) ->
+            System.String.Join(",", negativeAddends)
+            |> sprintf "Negatives not allowed (%s)" 
+
+        let private checkForNegativeNumbers = fun(addends:int list) ->
+            let negativeAddend = fun(addend) -> addend < 0 
+            if (List.exists negativeAddend addends) 
+                then invalidArg "Negative" (negativeNumberExceptionMessage (List.filter negativeAddend addends))
+            addends
+
+        let private onlyNumbersUnderOneThousand = fun(addend:int) -> addend < 1000 
+
+        let Add = 
+            fun (addends:string) -> 
+                match addends with
+                | "" -> 0
+                | _ -> Parse(addends)
+                       |> checkForNegativeNumbers
+                       |> List.filter onlyNumbersUnderOneThousand
+                       |> Seq.sum
+
---
+++ adder_new_lang/dougmorgan/StringCalculator/Adder/ParserTests.fs
@@ -0,0 +1,21 @@
+﻿module ParserTests
+
+    open NUnit.Framework
+    open FsUnit
+    open StringCalculator.Parser
+
+    [<TestFixture>]
+    type ParserTests() = class
+
+        [<Test>]
+        member x.``should handle comma delimiter" ``() = 
+            Assert.That(Parse("1,2,4,5"), Is.EqualTo([1;2;4;5;]))
+
+        [<Test>]
+        member x.``should handle newline delimiter" ``() = 
+            Assert.That(Parse("1\n2\n4\n5"), Is.EqualTo([1;2;4;5;]))
+
+        [<Test>]
+        member x.``should be able to override delimiter" ``() = 
+            Assert.That(Parse("//;\n1;2;4;5"), Is.EqualTo([1;2;4;5;]))
+    end
