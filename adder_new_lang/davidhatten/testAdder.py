b0b41491c65a25a40066a3a8d2dfaf143f553279
davidhatten <david.r.hatten@gmail.com>
2015-09-01 13:52:01 -0400

    added test to add no numbers

--- adder_new_lang/davidhatten/testAdder.py
+++ adder_new_lang/davidhatten/testAdder.py
@@ -1,4 +1,12 @@
 __author__ = 'david'
 import unittest
+import adder
 
 
+class AdderTestCase(unittest.TestCase):
+    def test_addNoNumbers(self):
+        result = adder.add()
+        self.assertEqual(0, result, "Adding no parameters should result in 0")
+
+if __name__ == '__main__':
+    unittest.main()
