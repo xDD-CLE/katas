f005105c7bd57bf9fe690d68005325d2745fc47a
Doug Morgan <doug.morgan@leandogsoftware.com>
2015-09-01 13:37:01 -0400

    Refactoring For Fun and Profit. Added Fake tasks to handle build.

--- adder_new_lang/dougmorgan/StringCalculator/Adder/Parser.fs
+++ adder_new_lang/dougmorgan/StringCalculator/Adder/Parser.fs
@@ -1,27 +1,34 @@
 ﻿namespace StringCalculator
     module Parser =
 
-        let DEFAULT_DELIMITER = ','
-        let NEWLINE_CHARACTER = '\n'
+        let DEFAULT_DELIMITER = ","
+        let NEWLINE_CHARACTER = "\n"
 
+        let private replaceDelimiterWithDefault = fun (delimiter:string)(addends:string) ->
+            addends.Replace(delimiter, DEFAULT_DELIMITER)
+
+        let private getDelimiterOverride = fun(addends:string) ->
+            addends.Substring(2,1)
+            
         let private replaceDelimiterOverride = fun(addends:string) -> 
-            let delimiterOverride = addends.Substring(2,1)
-            addends
-                .Substring(4)
-                .Replace((char) delimiterOverride, DEFAULT_DELIMITER)
+             addends.Substring(4)
+            |> replaceDelimiterWithDefault (getDelimiterOverride addends)
 
         let private delimiterOverridden = fun(addends:string) ->
              let delimiterOverrideKey = "//"
              addends.StartsWith(delimiterOverrideKey)
     
-        let private handleNewDelimiter = fun(addends:string) ->
+        let private SplitOnDefault = fun(addends:string) -> 
+            addends.Split((char)DEFAULT_DELIMITER)
+            |> Array.toList
+         
+        let private NormalizeDelimiter = fun(addends:string) ->
             match delimiterOverridden(addends) with
                 | true -> replaceDelimiterOverride(addends)
-                | false -> addends
+                | false -> replaceDelimiterWithDefault NEWLINE_CHARACTER addends
 
         let Parse = fun (addends:string) ->
-            handleNewDelimiter(addends)
-                .Replace(NEWLINE_CHARACTER, DEFAULT_DELIMITER)
-                .Split(DEFAULT_DELIMITER)
-           |> Array.toList
+           addends
+           |> NormalizeDelimiter
+           |> SplitOnDefault
            |> List.map int
