85a70d6e5bde5083fdf2994c84b428f4dc982891
Dave Shah <dave.shah@genomoncology.com>
2015-09-01 06:34:39 -0400

    Scaffolding

---
+++ adder_new_lang/daveshah/README.md
@@ -0,0 +1,9 @@
+## This is my kata. There are many like it but this one is mine. 
+
+It's a Clojure library designed to help me grok Clojure :)
+
+## Usage
+
+```brew install leiningen```
+```lein test```
+
---
+++ adder_new_lang/daveshah/test/daveshah/core_test.clj
@@ -0,0 +1,13 @@
+(ns daveshah.core-test
+  (:require [clojure.test :refer :all]
+            [daveshah.core :refer :all]))
+
+(deftest a-string-calculator
+
+  (testing "FIXME, I fail."
+    (is (= 0 1)))
+
+  (testing "FIXME, I still fail."
+    (is (= 0 1)))
+
+  )
