de19788707bf6ad9f66d8d4c07ab19f1c4b99851
Garrett Openshaw <garrett@Garretts-MacBook-Pro.local>
2015-09-03 16:50:12 -0400

    Using minitest!

--- adder_new_lang/gopenshaw/adder.rb
+++ adder_new_lang/gopenshaw/adder.rb
@@ -1,10 +1,9 @@
-require 'neo'
+require 'minitest/spec'
+require 'minitest/autorun'
 
-class Adder < Neo::Koan
+class Adder
 
-# Code #
-
-	def add(input)
+	def self.add(input)
 		numbers = parse(input)
 		sum = 0
 		numbers.each do |number|
@@ -13,7 +12,7 @@ class Adder < Neo::Koan
 		return sum
 	end
 
-	def parse(input)
+	def self.parse(input)
 		if (input[0,2] == "//")
 			delimiter = input[2, 1]
 			return input[4,input.length].split(delimiter)
@@ -21,30 +20,31 @@ class Adder < Neo::Koan
 
 		return input.split(%r',|\n')
 	end
+end
 
-# Tests #
+class AdderTests < MiniTest::Unit::TestCase
 
 	def test_sum_of_no_numbers_is_zero
-		assert_equal 0, add("")
+		assert_equal 0, Adder.add("")
 	end
 
 	def test_a_single_number_is_itself
-		assert_equal 1, add("1")
+		assert_equal 1, Adder.add("1")
 	end
 
 	def test_it_sums_two_numbers
-		assert_equal 21, add("5,16")
+		assert_equal 21, Adder.add("5,16")
 	end
 
 	def test_it_sums_many_numbers
-		assert_equal 39, add("5,16,12,1,5")
+		assert_equal 39, Adder.add("5,16,12,1,5")
 	end
 
 	def test_numbers_separated_by_newlines
-		assert_equal 6, add("1\n2,3")
+		assert_equal 6, Adder.add("1\n2,3")
 	end
 
 	def test_supports_custom_delimiters
-		assert_equal 8, add("//;\n1;5;2")
+		assert_equal 8, Adder.add("//;\n1;5;2")
 	end
 end
