87379023d63060e5ef560d6f6b5d3c478f601969
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2015-09-02 22:15:48 -0400

    It splits on commas and newlines

--- adder_new_lang/gopenshaw/adder.rb
+++ adder_new_lang/gopenshaw/adder.rb
@@ -2,7 +2,7 @@ require 'neo'
 
 class Adder < Neo::Koan
 	def add(input)
-		numbers = input.split(',')
+		numbers = input.split(%r',|\n')
 		sum = 0
 		numbers.each do |number|
 			sum += Integer(number)
@@ -20,5 +20,13 @@ class Adder < Neo::Koan
 
 	def test_it_sums_two_numbers
 		assert_equal 21, add("5,16")
+	end
+
+	def test_it_sums_many_numbers
+		assert_equal 39, add("5,16,12,1,5")
+	end
+
+	def test_numbers_separated_by_newlines
+		assert_equal 6, add("1\n2,3")
 	end
 end
