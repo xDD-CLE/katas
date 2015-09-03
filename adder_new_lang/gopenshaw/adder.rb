a45f3e81d30f578786c233cda5cb3fecdf92ca94
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2015-09-02 22:36:40 -0400

    It supports custom delimiters

--- adder_new_lang/gopenshaw/adder.rb
+++ adder_new_lang/gopenshaw/adder.rb
@@ -1,14 +1,28 @@
 require 'neo'
 
 class Adder < Neo::Koan
+
+# Code #
+
 	def add(input)
-		numbers = input.split(%r',|\n')
+		numbers = parse(input)
 		sum = 0
 		numbers.each do |number|
 			sum += Integer(number)
 		end
 		return sum
 	end
+
+	def parse(input)
+		if (input[0,2] == "//")
+			delimiter = input[2, 1]
+			return input[4,input.length].split(delimiter)
+		end
+
+		return input.split(%r',|\n')
+	end
+
+# Tests #
 
 	def test_sum_of_no_numbers_is_zero
 		assert_equal 0, add("")
@@ -28,5 +42,9 @@ class Adder < Neo::Koan
 
 	def test_numbers_separated_by_newlines
 		assert_equal 6, add("1\n2,3")
+	end
+
+	def test_supports_custom_delimiters
+		assert_equal 8, add("//;\n1;5;2")
 	end
 end
