9673a2600f56f3671253f3eff58be0b4be573ea7
davidhatten <david.r.hatten@gmail.com>
2015-09-03 14:57:47 -0400

    added newline

--- adder_new_lang/davidhatten/haskell/adder.hs
+++ adder_new_lang/davidhatten/haskell/adder.hs
@@ -1,7 +1,9 @@
 module Adder where
 import Data.List.Split
 
+delimiters = ",\n"
+
 add :: String -> Int
 add [] = 0
 add (x:[]) = read [x]
-add (x) = sum $ map read $ splitOn "," x :: Int
+add (x) = sum $ map read $ splitOneOf delimiters x :: Int
--- adder_new_lang/davidhatten/haskell/testAdder.hs
+++ adder_new_lang/davidhatten/haskell/testAdder.hs
@@ -4,7 +4,10 @@ import Adder
 
 testAddEmptyString = TestCase $ assertEqual "An empty string should add to 0" 0 ( add "" )
 testAddSingleNumber = TestCase $ assertEqual "2 should add to 2" 2 ( add "2" )
-testAddMoreThanOneNumber = TestCase $ assertEqual "2 plus 2 is 4" 4 ( add "2,2" )
+testAddMoreThanOneNumber = TestCase $ do
+                            assertEqual "2 plus 2 is 4" 4 ( add "2,2" )
+                            assertEqual "3 plus 3 plus 3 is 9" 9 ( add "3,3,3" )
+testAddNumbersWithNewlineDelimiter = TestCase $ assertEqual "3 plus 3 plus 3 is 6, split with a newline" 9 ( add "3,3\n3")
 
 
-main = runTestTT $ TestList [ testAddEmptyString, testAddSingleNumber, testAddMoreThanOneNumber ]
+main = runTestTT $ TestList [ testAddEmptyString, testAddSingleNumber, testAddMoreThanOneNumber, testAddNumbersWithNewlineDelimiter ]
