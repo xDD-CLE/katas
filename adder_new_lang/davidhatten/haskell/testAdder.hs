0cc24e49c33c919879f7a48483d582c69a11479e
davidhatten <david.r.hatten@gmail.com>
2015-09-03 17:18:10 -0400

    test for negative fail. \n\n can't figure out how to reach into exceptions to check text, but figured out how to check for exceptions. The next step is putting a guard in one of the methods to check for negatives before passing it on to be summed.

--- adder_new_lang/davidhatten/haskell/testAdder.hs
+++ adder_new_lang/davidhatten/haskell/testAdder.hs
@@ -1,4 +1,6 @@
 module TestAdder where
+import Control.Exception
+import Control.Monad
 import Test.HUnit
 import Adder
 
@@ -9,6 +11,15 @@ testAddMoreThanOneNumber = TestCase $ do
                             assertEqual "3 plus 3 plus 3 is 9" 9 ( add "3,3,3" )
 testAddNumbersWithNewlineDelimiter = TestCase $ assertEqual "3 plus 3 plus 3 is 6, split with a newline" 9 ( add "3,3\n3")
 testCustomDelimiter = TestCase $ assertEqual "2 plus 2 is 4 with custom delimiters" 4 ( add "//:\n2:2" )
+testNegativeNumberFail = TestCase $ do
+                    handleJust isWanted (const $ return ()) $ do
+                        ( evaluate $ add "2,-3,4" )
+                        assertFailure "Negative values should throw errors"
+                    where isWanted = guard . (== LossOfPrecision) 
 
-
-main = runTestTT $ TestList [ testAddEmptyString, testAddSingleNumber, testAddMoreThanOneNumber, testAddNumbersWithNewlineDelimiter, testCustomDelimiter ]
+main = runTestTT $ TestList [ testAddEmptyString, 
+                                testAddSingleNumber, 
+                                testAddMoreThanOneNumber, 
+                                testAddNumbersWithNewlineDelimiter, 
+                                testCustomDelimiter,
+                                testNegativeNumberFail ]
