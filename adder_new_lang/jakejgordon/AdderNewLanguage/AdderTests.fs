398b33d2d6ea11b46d72495ea3ec168de0013b2e
Jake Gordon <jacob.gordon@Emerson.com>
2015-09-08 21:46:49 -0400

    blank should return 0

---
+++ adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
@@ -0,0 +1,15 @@
+﻿module AdderTests
+
+open NUnit.Framework
+open FsUnit
+open Adder
+
+[<TestFixture>]
+type AdderTests() =
+    let adder = new Adder()
+
+    [<Test>] 
+    let ``Given I'm adding a blank string``() =
+       adder.AddDelimitedIntegers "" |> should equal 0
+
+
