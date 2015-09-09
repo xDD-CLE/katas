efd4f0d5509e2ccd195ee047cf131e3b0cccaec6
Jake Gordon <jacob.gordon@Emerson.com>
2015-09-08 21:57:00 -0400

    single integers are returned

--- adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
+++ adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
@@ -10,4 +10,10 @@ let adder = new Adder()
 let ``Given I'm adding a blank string``() =
    adder.AddDelimitedIntegers "" |> should equal 0
 
+[<Test>]
+let ``Given I'm adding a single integer (1)``() =
+    adder.AddDelimitedIntegers "1" |> should equal 1
 
+[<Test>]
+let ``Given I'm adding a single integer (2)``() =
+    adder.AddDelimitedIntegers "2" |> should equal 2
