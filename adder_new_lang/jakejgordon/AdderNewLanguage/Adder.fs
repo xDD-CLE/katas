17eae4ed2a751652bedb9d44b086a1c00e713d08
Jake Gordon <jacob.gordon@Emerson.com>
2015-09-09 07:21:42 -0400

    minor refactor

--- adder_new_lang/jakejgordon/AdderNewLanguage/Adder.fs
+++ adder_new_lang/jakejgordon/AdderNewLanguage/Adder.fs
@@ -5,6 +5,8 @@ open System
 
 
 type Adder() =
-    member this.ReturnZeroIfBlank delimitedString = if delimitedString = "" then 0 else Int32.Parse(delimitedString)
+    member this.ReturnZeroIfBlank delimitedString = if delimitedString = "" then "0" else delimitedString
 
-    member this.AddDelimitedIntegers delimitedString = this.ReturnZeroIfBlank delimitedString
+    member this.ReturnInteger delimitedString = Int32.Parse(delimitedString)
+
+    member this.AddDelimitedIntegers delimitedString = this.ReturnZeroIfBlank delimitedString |> this.ReturnInteger
--- adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
+++ adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
@@ -17,3 +17,7 @@ let ``Given I'm adding a single integer (1)``() =
 [<Test>]
 let ``Given I'm adding a single integer (2)``() =
     adder.AddDelimitedIntegers "2" |> should equal 2
+
+[<Test>]
+let ``Given I'm adding two integers (1,1)``() =
+    adder.AddDelimitedIntegers "1,1" |> should equal 2
