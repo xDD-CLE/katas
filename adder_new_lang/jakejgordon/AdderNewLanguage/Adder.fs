c02d0df1949c44f8a1bb4c6a3a1eaff406b31283
Jake Gordon <jacob.gordon@Emerson.com>
2015-09-08 21:55:31 -0400

    fixed test and remove fixture
    ... in favor of just doing module-level bindings

--- adder_new_lang/jakejgordon/AdderNewLanguage/Adder.fs
+++ adder_new_lang/jakejgordon/AdderNewLanguage/Adder.fs
@@ -7,4 +7,4 @@ open System
 type Adder() =
     member this.ReturnZeroIfBlank delimitedString = if delimitedString = "" then 0 else Int32.Parse(delimitedString)
 
-    member public this.AddDelimitedIntegers delimitedString = this.ReturnZeroIfBlank delimitedString
+    member this.AddDelimitedIntegers delimitedString = this.ReturnZeroIfBlank delimitedString
--- adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
+++ adder_new_lang/jakejgordon/AdderNewLanguage/AdderTests.fs
@@ -4,12 +4,10 @@ open NUnit.Framework
 open FsUnit
 open Adder
 
-[<TestFixture>]
-type AdderTests() =
-    let adder = new Adder()
+let adder = new Adder()
 
-    [<Test>] 
-    let ``Given I'm adding a blank string``() =
-       adder.AddDelimitedIntegers "" |> should equal 0
+[<Test>] 
+let ``Given I'm adding a blank string``() =
+   adder.AddDelimitedIntegers "" |> should equal 0
 
 
