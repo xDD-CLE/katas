56b1775ee3c59c0b79199cb2763f23835080f286
Sam Jones <sam@samljones.com>
2015-11-20 23:37:21 -0500

    refactoring trigramerator to abstract implementation better

--- trigrams/samjones/rubyOO/lib/simple_trigramerator.rb
+++ trigrams/samjones/rubyOO/lib/simple_trigramerator.rb
@@ -2,11 +2,24 @@ class SimpleTrigramerator
 	def initialize(tokenizer)
 		@tokenizer = tokenizer
 	end
-	def trigramerate
-		{}.tap do |hash| 
+	def trigramerate!
+		@trigrameration = {}.tap do |hash| 
 			@tokenizer.words.each_cons(3) do |trigram|
 				(hash[trigram.first(2).join(' ')] ||= []) << trigram[2]
 			end
 		end
+	end
+	def seed
+		check_state
+		@trigrameration.keys.sample
+	end
+	def retrieve(key)
+		check_state
+		@trigrameration[key]
+	end
+	
+	private
+	def check_state
+		raise 'You must first call trigramerate! to populate your trigramerations' if !defined?(@trigrameration)
 	end
 end
--- trigrams/samjones/rubyOO/spec/simple_trigramerator_spec.rb
+++ trigrams/samjones/rubyOO/spec/simple_trigramerator_spec.rb
@@ -2,15 +2,28 @@ require 'simple_trigramerator'
 
 describe "SimpleTrigramerator" do
 	context "when I trigramerate a tokenizer" do
-		it "should find followers" do
+		it "should populate trigramerations" do
 			tokenizer = double('Tokenizer', words: ['I', 'wish', 'I', 'may', 'I', 'wish', 'I', 'might'])
 			trigramerator = SimpleTrigramerator.new(tokenizer)
-			expect(trigramerator.trigramerate).to eq({
-				"I wish" => ["I", "I"],
-				"wish I" => ["may", "might"],
-				"may I"  => ["wish"],
-				"I may"  => ["I"]
-			})
+
+			trigramerator.trigramerate!
+
+			expect(trigramerator.seed).to_not be_empty
+
+			expect(trigramerator.retrieve("I wish")).to eq(["I", "I"])
+			expect(trigramerator.retrieve("wish I")).to eq(["may", "might"])
+			expect(trigramerator.retrieve("may I")).to eq( ["wish"])
+			expect(trigramerator.retrieve("I may")).to eq( ["I"])
+		end
+	end
+	context "when I don't trigramerate a tokenizer" do
+		it "should raise an error" do
+			trigramerator = SimpleTrigramerator.new(double('Tokenizer'))
+
+expected_error = 'You must first call trigramerate! to populate your trigramerations'
+
+			expect{trigramerator.seed}.to raise_error(expected_error)
+			expect{trigramerator.retrieve('foo')}.to raise_error(expected_error)
 		end
 	end
 end
