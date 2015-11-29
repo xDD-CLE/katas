0d596e19729e5fc37f09a1a230313d25faac6614
Nathan Wallace <nathan.m.wallace@gmail.com>
2015-11-29 09:23:13 -0500

    add ability to blather on using trigrams

--- trigrams/nathan/tom_swift.clj
+++ trigrams/nathan/tom_swift.clj
@@ -1,14 +1,34 @@
 (ns tom-swift
   (:require [clojure.string :as string]))
 
-(defn -build-trigrams
+(defn- build-trigrams
   ([strings]
-   (-build-trigrams {} strings))
+   (build-trigrams {} strings))
+
   ([trigrams [a b c & more]]
    (if c
-     (-build-trigrams
+     (build-trigrams
        (merge-with concat trigrams {(str a " " b) [c]}) (concat [b c] more))
      trigrams)))
 
+(defn- trigram-sequence
+  ([trigrams iterations seed]
+   (trigram-sequence trigrams iterations seed seed))
+
+  ([trigrams iterations seed blathering]
+   (let [new-word (rand-nth (get trigrams seed))
+         new-seed (str (last (string/split seed #"\s+")) " " new-word)]
+     (if (and new-word (pos? iterations))
+       (trigram-sequence
+         trigrams (- iterations 1) new-seed (str blathering " " new-word))
+       blathering))))
+
 (defn trigrams-for [document]
-  (-build-trigrams (string/split document #"\s+")))
+  (build-trigrams (string/split document #"\s+")))
+
+(defn blather [document]
+  (let [trigrams (trigrams-for document)
+        seed (rand-nth (keys trigrams))]
+    (if seed
+      (trigram-sequence trigrams 5 seed)
+      "")))
--- trigrams/nathan/tom_swift_test.clj
+++ trigrams/nathan/tom_swift_test.clj
@@ -3,6 +3,9 @@
 
 (load-file "tom_swift.clj")
 
+(def expected-trigrams
+  {"I wish" ["I" "I"] "wish I" ["may" "might"] "I may" ["I"] "may I" ["wish"]})
+
 (deftest no-trigrams
   (is (= {} (tom-swift/trigrams-for "I wish"))))
 
@@ -14,7 +17,27 @@
          (tom-swift/trigrams-for "I wish I may"))))
 
 (deftest more-multiple-trigrams
-  (is (= {"I wish" ["I" "I"] "wish I" ["may" "might"] "I may" ["I"] "may I" ["wish"]}
+  (is (= expected-trigrams
          (tom-swift/trigrams-for "I wish I may I wish I might"))))
+
+(deftest no-blathering
+  (is (= "" (tom-swift/blather "I wish"))))
+
+(deftest simple-blathering
+  (is (= "I wish I" (tom-swift/blather "I wish I"))))
+
+(defn note [desc data]
+  (do
+    (println "[NOTE] " desc ": " data)
+    data))
+
+(deftest aphasic-blathering
+  (is (every?
+        (fn [[a b c]]
+          (contains? (set (get expected-trigrams (str a " " b))) c))
+        (partition
+          3 1
+          (clojure.string/split
+            (tom-swift/blather "I wish I may I wish I might") #"\s+" )))))
 
 (run-tests)
