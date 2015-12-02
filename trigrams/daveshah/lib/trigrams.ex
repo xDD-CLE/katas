34485d3c7b6515e6528236a2890181a87aa29881
Dave Shah <dave.shah@genomoncology.com>
2015-12-02 06:27:39 -0500

    Introduce TrigramHelpers

--- trigrams/daveshah/lib/trigrams.ex
+++ trigrams/daveshah/lib/trigrams.ex
@@ -1,7 +1,25 @@
 defmodule Trigrams do
 
   def from(text) do
+    threes = String.split(text, " ") 
+              |> Enum.chunk(3,1)
+    keys = Enum.map(threes, &Enum.at(&1, 0))
+    vals = Enum.map(threes, &Enum.drop(&1, 1))
+    
     %{}
   end
+
+end
+
+defmodule TrigramHelpers do
+
+  def break_into_threes(text) do
+    String.split(text, " ") |> Enum.chunk(3,1)
+  end
+
+  def create_keys_from(threes) do
+    Enum.map(threes, &Enum.at(&1,0)) |> Enum.uniq
+  end
+
 
 end
--- trigrams/daveshah/test/trigrams_test.exs
+++ trigrams/daveshah/test/trigrams_test.exs
@@ -7,4 +7,56 @@ defmodule TrigramsTest do
     trigrams = Trigrams.from(simple_sentance)
     assert is_map(trigrams)
   end
+  
+
+  #  test "that the keys within the map are 2 word pairs" do
+  #    simple_sentance = "I wish I may I wish I might"
+  #    trigrams = Trigrams.from(simple_sentance)
+  #
+  #    keys = Map.keys(trigrams)
+  #
+  #    assert Enum.any?(keys, fn k -> k == "I wish" end)
+  #    assert Enum.any?(keys, fn k -> k == "wish I" end)
+  #    assert Enum.any?(keys, fn k -> k == "I may" end)
+  #    assert Enum.any?(keys, fn k -> k == "may I" end)
+  #  end
+end
+
+defmodule TrigramHelpersTest do
+  use ExUnit.Case
+  doctest TrigramHelpers
+
+  test "that it splits the text into groups of three" do
+    simple_sentance = "I wish I may I wish I might"
+    broken_sentence = TrigramHelpers.
+                      break_into_threes(simple_sentance)
+    assert broken_sentence == [["I", "wish", "I"],
+                              ["wish", "I", "may"],
+                              ["I", "may", "I"],
+                              ["may", "I", "wish"],
+                              ["I", "wish", "I"],
+                              ["wish", "I", "might"]]
+
+  end
+
+  test "that it gives me keys based on the first element of each split of three" do
+
+    broken_sentence = [["I", "wish", "I"],
+                      ["wish", "I", "may"],
+                      ["I", "may", "I"],
+                      ["may", "I", "wish"],
+                      ["I", "wish", "I"],
+                      ["wish", "I", "might"]]
+
+    keys = TrigramHelpers.create_keys_from(broken_sentence)
+
+    assert keys == ["I", "wish", "may"]
+
+  end
+
+
+
+
+
+
 end
