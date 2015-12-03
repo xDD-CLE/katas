78cc287b29d8da58acdc63d1907b97310bcea4dc
Dave Shah <dave.shah@genomoncology.com>
2015-12-02 22:09:07 -0500

    Inlines

--- trigrams/daveshah/lib/trigrams.ex
+++ trigrams/daveshah/lib/trigrams.ex
@@ -1,27 +1,23 @@
 defmodule Trigrams do
 
   def from(text) do
-    threes = String.split(text, " ") 
-              |> Enum.chunk(3,1)
+    threes = String.split(text, " ") |> Enum.chunk(3,1)
     keys = Enum.map(threes, &Enum.at(&1, 0))
     vals = Enum.map(threes, &Enum.drop(&1, 1))
-    key_val_tupples = Enum.zip(keys,vals)
 
-    map_with_keys = Enum.reduce(keys, %{}, 
-                  fn (key, map) -> Dict.put(map,key,[]) 
-                  end
-                )
-    Enum.reduce(key_val_tupples, map_with_keys,
-                  fn(tupple, map) ->
-                    Dict.update!(map, elem(tupple,0), 
-                      fn(arr) ->
-                        arr ++ elem(tupple,1) |> Enum.uniq
-                      end
-                      )
-                  end
-                )
+    Enum.reduce(
+      Enum.zip(keys,vals), 
+      Enum.reduce(keys, %{}, 
+            fn(key,map) -> Dict.put(map,key,[]) end),
+        fn(tupple, map) ->
+          Dict.update!(map, elem(tupple,0), 
+            fn(arr) ->
+              arr ++ elem(tupple,1) |> Enum.uniq
+            end
+            )
+        end
+      )
   end
-
 end
 
 
