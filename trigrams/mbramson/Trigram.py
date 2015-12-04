194351e3ee7a158c0a688c1f699cb99f68722dd3
mbramson <mathewbramson@gmail.com>
2015-12-03 21:03:47 -0500

    refactor parse to act on class property

--- trigrams/mbramson/Trigram.py
+++ trigrams/mbramson/Trigram.py
@@ -3,15 +3,15 @@ from collections import Counter
 class Trigram:
     def __init__(self, input_text=''):
         self.input_text = input_text
+        self.map = {}
 
     def parse(self):
         words = self.input_text.split(' ')
         if len(words) < 3:
             raise ValueError('Input text must be at least 3 words in length')
-        return_dict = {}
+        self.map = {}
         for i in xrange(len(words) - 2):
             current_bigram = words[i] + ' ' + words[i+1]
-            if not return_dict.has_key(current_bigram):
-                return_dict[current_bigram] = Counter()
-            return_dict[current_bigram][words[i+2]] += 1
-        return return_dict
+            if not self.map.has_key(current_bigram):
+                self.map[current_bigram] = Counter()
+            self.map[current_bigram][words[i+2]] += 1
--- trigrams/mbramson/test_trigram.py
+++ trigrams/mbramson/test_trigram.py
@@ -15,10 +15,15 @@ class Test_Trigram(unittest.TestCase):
         trigram = Trigram()
         self.assertEqual(trigram.input_text, '')
 
+    def test_trigram_constructor_instantiates_empty_trigram_map_dict(self):
+        trigram = Trigram()
+        self.assertEqual(trigram.map, {})
+
     # test parse functionality
-    def test_parse_returns_dictionary(self):
+    def test_parse_makes_trigram_map_a_dictionary(self):
         trigram = Trigram('the quick brown fox jumped over the fence')
-        self.assertIsInstance(trigram.parse(), dict)
+        trigram.parse()
+        self.assertIsInstance(trigram.map, dict)
 
     def test_parse_raises_error_if_text_has_less_than_three_words(self):
         trigram = Trigram('two words')
@@ -26,24 +31,26 @@ class Test_Trigram(unittest.TestCase):
 
     def test_parse_adds_dictionary_entry_of_first_two_words(self):
         trigram = Trigram('three whole words')
-        return_dict = trigram.parse()
-        self.assertEqual('three whole', return_dict.keys()[0])
+        trigram.parse()
+        self.assertEqual('three whole', trigram.map.keys()[0])
 
     def test_parse_return_dict_values_are_counters(self):
         trigram = Trigram('three whole words')
-        self.assertIsInstance(trigram.parse()['three whole'], Counter)
+        trigram.parse()
+        self.assertIsInstance(trigram.map['three whole'], Counter)
 
     def test_parse_return_dict_has_second_bigram_as_key(self):
         trigram = Trigram('four whole real words')
-        self.assertIsInstance(trigram.parse()['whole real'], Counter)
+        trigram.parse()
+        self.assertIsInstance(trigram.map['whole real'], Counter)
 
     def test_parse_return_dict_value_counters_increment_to_1(self):
         trigram = Trigram('three whole words')
-        return_dict = trigram.parse()
-        self.assertEqual(1, return_dict['three whole']['words'])
+        trigram.parse()
+        self.assertEqual(1, trigram.map['three whole']['words'])
 
     def test_parse_return_dict_value_counters_increment_to_2(self):
         trigram = Trigram('three whole words and three whole words')
-        return_dict = trigram.parse()
-        self.assertEqual(2, return_dict['three whole']['words'])
+        trigram.parse()
+        self.assertEqual(2, trigram.map['three whole']['words'])
 
