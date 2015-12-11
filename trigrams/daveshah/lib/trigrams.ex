c60ce08decb7250fd80efa939dd1e54289d5a284
Dave Shah <dave.shah@genomoncology.com>
2015-12-10 23:26:04 -0500

    Use imports

--- trigrams/daveshah/lib/trigrams.ex
+++ trigrams/daveshah/lib/trigrams.ex
@@ -1,17 +1,20 @@
 defmodule Trigrams do
+  import Enum
+  import Dict
+  import List
 
   def from(text) do
     String.split(text, ~r{\W}, trim: true) 
-    |> Enum.chunk(3,1) 
-    |> Enum.map(&Enum.reverse(&1))
-    |> Enum.reduce(%{}, 
-        fn(arr,dict) -> 
-          Dict.update(dict, Enum.join(Enum.reverse(tl(arr))," "), [hd(arr)], 
-            fn(current) -> 
-              List.flatten(current ++  [hd(arr)]) 
-            end
-           )
-        end
+    |> chunk(3,1) 
+    |> map(&reverse(&1))
+    |> reduce(%{}, 
+      fn(array, dict_accumulator) -> 
+        update(dict_accumulator, tl(array) |> reverse |> join(" "), [hd(array)], 
+          fn(current) -> 
+            flatten(current ++  [hd(array)]) 
+          end
+        )
+      end
     )
   end
 
