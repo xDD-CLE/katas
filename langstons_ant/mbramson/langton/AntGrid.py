b0262ac3187b370f635d56514544e36db09de38d
mbramson <mathewbramson@gmail.com>
2015-12-17 22:09:54 -0500

    add grid_list to AntGrid

--- langstons_ant/mbramson/langton/AntGrid.py
+++ langstons_ant/mbramson/langton/AntGrid.py
@@ -1,7 +1,8 @@
-
+from langton.Square import Square
 
 class AntGrid:
     def __init__(self):
         self.x = 0
         self.y = 0
         self.dir = "up"
+        self.grid_list = [Square(x=0, y=0)]
--- langstons_ant/mbramson/langton/tests/test_ant.py
+++ langstons_ant/mbramson/langton/tests/test_ant.py
@@ -1,19 +1,27 @@
 import unittest
 
 from langton.AntGrid import AntGrid
+from langton.Square import Square
 
 
 class TestAnt(unittest.TestCase):
 
-    def test_ant_starts_at_0_x_coordinate(self):
+    def test_ant_grid_starts_at_0_x_coordinate(self):
         ant = AntGrid()
         self.assertEqual(0, ant.x)
 
-    def test_ant_starts_at_0_y_coordinate(self):
+    def test_ant_grid_starts_at_0_y_coordinate(self):
         ant = AntGrid()
         self.assertEqual(0, ant.y)
 
-    def test_ant_starts_with_up_direction(self):
+    def test_ant_grid_starts_with_up_direction(self):
         ant = AntGrid()
         self.assertEqual("up", ant.dir)
 
+    def test_ant_grid_has_grid_set(self):
+        ant = AntGrid()
+        self.assertIsInstance(ant.grid_list, list)
+
+    def test_ant_grid_contains_0_0_square(self):
+        ant = AntGrid()
+        self.assertIn(Square(x=0, y=0), ant.grid_list)
