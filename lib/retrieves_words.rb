1aa14eed592b7a57f27f65b5d72a1d85a1a503f7
Sam Jones <sam@samljones.com>
2016-01-14 12:38:43 -0500

    adding blacklist to retrieves words

--- lib/retrieves_words.rb
+++ lib/retrieves_words.rb
@@ -8,12 +8,14 @@ class RetrievesWords
 		@reads_dictionary = opts[:reads_dictionary] || ReadsDictionary.new
 	end
 
-	def next_word(start_word)
+	def next_word(start_word, opts = {})
+		blacklist = opts[:blacklist] || []
+
 		next_word = start_word.dup
 		(0..next_word.length-1).each do |i|
 			@@alphabet.each do |todays_letter|
 				next_word[i] = todays_letter
-				return next_word if @reads_dictionary.contains_word(next_word)
+				return next_word if @reads_dictionary.contains_word(next_word) and not blacklist.include?(next_word)
 			end
 			next_word[i] = start_word[i]
 		end
--- spec/retrieves_words_spec.rb
+++ spec/retrieves_words_spec.rb
@@ -1,17 +1,35 @@
 require 'retrieves_words'
 
 describe RetrievesWords do
-	let(:start_word) { 'ruby' }
-	let(:next_word) { 'rube' }
-
 	let(:reads_dictionary) { double('reads_dictionary') }
 
 	subject { RetrievesWords.new(reads_dictionary: reads_dictionary) }
 
-	it "Should retrieve the next word" do
-		expect(reads_dictionary).to receive(:contains_word).with(next_word).and_return(true).once
-		expect(reads_dictionary).to receive(:contains_word).and_return(false).at_least(:once)
+	context "finding next word with dictionary" do
+		let(:start_word) { 'ruby' }
+		let(:next_word) { 'rube' }
 
-		expect(subject.next_word(start_word)).to eq(next_word)
+		it "Should retrieve the next word" do
+			expect(reads_dictionary).to receive(:contains_word).with(next_word).and_return(true).once
+			expect(reads_dictionary).to receive(:contains_word).and_return(false).at_least(:once)
+
+			expect(subject.next_word(start_word)).to eq(next_word)
+		end
+	end
+
+	context "filtering next word from blacklist" do
+		let(:blacklist) {[ 'foo', 'bar' ]}
+
+		let(:start_word) { 'poo' }
+		let(:blacklisted_word) { 'foo' }
+		let(:next_word) { 'loo' }
+
+		it "Should blacklist words" do
+			expect(reads_dictionary).to receive(:contains_word).with(next_word).and_return(true).once
+			expect(reads_dictionary).to receive(:contains_word).with(blacklisted_word).and_return(true).once
+			expect(reads_dictionary).to receive(:contains_word).and_return(false).at_least(:once)
+
+			expect(subject.next_word(start_word, blacklist: blacklist)).to eq(next_word)
+		end
 	end
 end
