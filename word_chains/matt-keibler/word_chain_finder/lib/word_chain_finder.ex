97084c5c8845f4f5995051c942eff6a03fd530ce
Matthew Keibler <matthew.keibler@gmail.com>
2016-01-23 22:34:33 -0500

    top level tests complete

--- word_chains/matt-keibler/word_chain_finder/lib/word_chain_finder.ex
+++ word_chains/matt-keibler/word_chain_finder/lib/word_chain_finder.ex
@@ -1,9 +1,28 @@
 defmodule WordChainFinder do
 
+	def run() do
+		graph = get_graph(&IO.gets/2, &WordGraphBuilder.build_graph/1)
+		words = get_input(&IO.gets/2)
+		path = get_path(&AStar.search/2, graph, words)
+		print_path(&IO.puts/2, path)
+	end
+
 	def get_input(input_function) do
 		start_word = input_function.("Enter the start word: ")
 		end_word = input_function.("Enter the end word: ")
 		{start_word, end_word}
 	end
-	
+
+	def get_graph(input_function, graph_function) do
+		graph_filename = input_function.("Enter the word list filename: ")
+		graph_function.(graph_filename)
+	end
+
+	def get_path(search_function, graph, words) do
+		search_function.(graph, words)
+	end
+
+	def print_path(output_function, path) do
+		path |> Enum.reduce(fn(token, acc) -> acc <> " -> " <> token end) |> output_function.()
+	end
 end
--- word_chains/matt-keibler/word_chain_finder/test/word_chain_finder_test.exs
+++ word_chains/matt-keibler/word_chain_finder/test/word_chain_finder_test.exs
@@ -10,5 +10,33 @@ defmodule WordChainFinderTest do
 		
 		{"cat", "dog"} == WordChainFinder.get_input(input_function)
 	end
+
+	test "it should accept a graph filename" do
+		input_function = fn text -> "graph_file" end
+		graph_function = fn text -> {:ok, %{"cat" => ["cot"]}} end
+
+		{:ok, %{"cat" => ["cot"]}} = WordChainFinder.get_graph(input_function, graph_function)
+	end
+
+	test "it should get a path from the search" do
+		search_function = fn
+			graph, words when graph == %{"cat" => "cot"} and words == {"cat", "dog"} -> ["cat", "cot", "dog"]
+		end
+		graph = %{"cat" => "cot"}
+		words = {"cat", "dog"}
+		
+		
+		["cat", "cot", "dog"] = WordChainFinder.get_path(search_function, graph, words)
+	end
+
+	test "it should print the returned path" do
+		output_function = fn
+			path when path == "cat -> cot -> dog" -> :ok
+		  _ -> :error
+		end
+		path = ["cat", "cot", "dog"]
+
+		:ok = WordChainFinder.print_path(output_function, path)
+	end
 	
 end
