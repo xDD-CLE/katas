f4e6d8e29771ea887cc9c4a65b6f8892b7c60537
Matthew Keibler <matthew.keibler@gmail.com>
2016-01-26 07:49:53 -0500

    Got word list from file

--- word_chains/matt-keibler/word_chain_finder/lib/word_graph_builder.ex
+++ word_chains/matt-keibler/word_chain_finder/lib/word_graph_builder.ex
@@ -1,7 +1,20 @@
 defmodule WordGraphBuilder do
 
 	def build_graph(filename) do
-
+		if File.exists? filename do
+			{:ok, file_content} = File.read(filename)
+			parse_content_to_list(file_content)
+		else
+			{:error, "File does not exist"}
+		end
 	end
 
+	defp parse_content_to_list(file_content) do
+		word_list = file_content |> String.split("\n") |> Enum.filter(&(String.length(&1) > 0))
+		if Enum.count(word_list) > 0 do
+			{:ok, word_list}
+		else
+			{:error, "File did not contain a word list."}
+		end
+	end
 end
