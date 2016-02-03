d502792220aa53df737280d59818c0d2db2e8c44
Matthew Keibler <matthew.keibler@gmail.com>
2016-02-03 17:26:44 -0500

    initial refactor pass

--- word_chains/matt-keibler/word_chain_finder/lib/word_distance.ex
+++ word_chains/matt-keibler/word_chain_finder/lib/word_distance.ex
@@ -3,16 +3,25 @@ defmodule WordDistance do
 	def neighbors?(left_word, right_word) do
 		if String.length(left_word) == String.length(right_word) do
 			1 == distance(left_word, right_word)
-		else
-			false
 		end
 	end
 
 	def distance(left_word, right_word) do
-		length_diff = abs(String.length(left_word) - String.length(right_word))
-		letter_diff = Enum.zip(String.to_char_list(left_word), String.to_char_list(right_word))
-		  |> Enum.map(fn({x,y} -> 0 != (y - x)) end)
-  	  |> Enum.reduce(0, fn(x, acc) -> if x do acc + 1 else acc end end)
-		length_diff + letter_diff
+		length_diff(left_word, right_word) + letter_diff(left_word, right_word)
 	end
+
+	defp length_diff(left_word, right_word) do
+		abs(String.length(left_word) - String.length(right_word))
+	end
+
+	defp letter_diff(left_word, right_word) do
+		Enum.zip(String.to_char_list(left_word), String.to_char_list(right_word))
+		|> Enum.map(&char_diff/1)
+  	|> Enum.reduce(0, &different?/2)
+	end
+
+	defp char_diff({left_char, right_char}) do 0 != (right_char - left_char) end
+
+	defp different?(true, num_different) do num_different + 1 end
+	defp different?(false, num_different) do num_different end
 end
