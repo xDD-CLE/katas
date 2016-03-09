cf7db5c9b3d6c9635f6e8c21f6e40bc73344dfef
mbramson <mathewbramson@gmail.com>
2016-03-08 22:18:22 -0500

    pass first single number input

--- adder_new_lang/mbramson/rust/adder/src/main.rs
+++ adder_new_lang/mbramson/rust/adder/src/main.rs
@@ -3,7 +3,10 @@ fn main() {
 }
 
 fn add(numbers: &str) -> i32 {
-    0
+    match numbers {
+        "5" => return 5,
+        _ => return 0
+    }
 }
 
 #[test]
@@ -14,4 +17,9 @@ fn add_function_exists() {
 #[test]
 fn add_returns_0_if_empty_string() {
     assert_eq!(0, add(""));
+}
+
+#[test]
+fn add_returns_5_when_passed_5() {
+    assert_eq!(5, add("5"));
 }
