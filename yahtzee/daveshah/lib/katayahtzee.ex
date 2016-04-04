ac6974161ec6c7e66c5979edb444104f93e7e7ac
Dave Shah <david.a.shah@gmail.com>
2016-04-03 21:45:02 -0400

    refactor pass

--- yahtzee/daveshah/lib/katayahtzee.ex
+++ yahtzee/daveshah/lib/katayahtzee.ex
@@ -1,7 +1,5 @@
 defmodule Katayahtzee do
 
-  @moduledoc
-
   def placement(dice_roll, placement) do
     dice_roll
     |> Enum.filter(&(&1 == placement))
@@ -10,27 +8,29 @@ defmodule Katayahtzee do
 
   def two_pairs(dice_roll) do
     dice_roll 
-    |> value_in_list_to_count 
-    |> Enum.filter_map(fn({k,v}) -> v == 2 end, fn({k,v}) -> k*2 end)
+    |> calculate_for_pairs
     |> Enum.sum
   end
 
   def pair(dice_roll) do
     dice_roll 
-    |> value_in_list_to_count 
-    |> sum_of_the_highest_count_value(2)
+    |> calculate_for_pairs
     |> Enum.max
   end
 
+  defp calculate_for_pairs(dice_roll) do
+    dice_roll 
+    |> value_in_list_to_count 
+    |> sum_of_the_highest_count_value(2)
+  end
+
   defp value_in_list_to_count(numbers) do
-    increment_count = &(&1 +1)
     numbers 
-    |> 
-    Enum.reduce(%{}, fn(number, map) -> Map.update(map, number, 1, increment_count) end)
+    |> Enum.reduce(%{}, fn(number, map) -> Map.update(map, number, 1, &(&1 + 1)) end)
   end
 
   defp sum_of_the_highest_count_value(value_to_count, filter_count) do
     value_to_count
-    |> Enum.filter_map(fn({_,v}) -> v == filter_count end, fn({k,_}) -> k*filter_count end)
+    |> Enum.filter_map(fn({_, v}) -> v == filter_count end, fn({k, _}) -> k*filter_count end)
   end
 end
