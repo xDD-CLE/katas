7294e70f6fc8298779171534f7342dc73b9ccd5a
Jerry D'Antonio <stumpjumper@gmail.com>
2016-04-10 16:37:33 -0400

    Simplest possible implementation of the 'Twos' rule.

--- yahtzee/jdantonio/lib/yahtzee.js
+++ yahtzee/jdantonio/lib/yahtzee.js
@@ -1,10 +1,34 @@
 /*eslint no-console: ["error", { allow: ["log"] }] */
 
-module.exports.score = function(rule, dice) {
+function onesScore(dice) {
   return dice.reduce(function(previousValue, currentValue/*, currentIndex, array*/) {
     if (currentValue === 1) {
       previousValue++;
     }
     return previousValue;
   }, 0);
+}
+
+function twosScore(dice) {
+  return dice.reduce(function(previousValue, currentValue/*, currentIndex, array*/) {
+    if (currentValue === 2) {
+      previousValue += 2;
+    }
+    return previousValue;
+  }, 0);
+}
+
+module.exports.score = function(rule, dice) {
+  var score = 0;
+
+  switch (rule.toLowerCase()) {
+  case 'ones':
+    score = onesScore(dice);
+    break;
+  case 'twos':
+    score = twosScore(dice);
+    break;
+  }
+
+  return score;
 };
--- yahtzee/jdantonio/test/yahtzee_test.js
+++ yahtzee/jdantonio/test/yahtzee_test.js
@@ -20,3 +20,24 @@ exports.testOnesRuleWithAllOnes = function(test) {
   test.equal(expected, actual);
   test.done();
 };
+
+exports.testTwosRuleWithZeroTwos = function(test) {
+  var expected = 0;
+  var actual = yahtzee.score('Twos', [1, 3, 4, 5, 6]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testTwosRuleWithSomeTwos = function(test) {
+  var expected = 4;
+  var actual = yahtzee.score('Twos', [1, 2, 3, 4, 2]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testTwosRuleWithAllTwos = function(test) {
+  var expected = 10;
+  var actual = yahtzee.score('Twos', [2, 2, 2, 2, 2]);
+  test.equal(expected, actual);
+  test.done();
+};
