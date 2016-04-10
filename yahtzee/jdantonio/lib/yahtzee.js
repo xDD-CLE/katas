279180e5c0d39907c1c7547986ddc8b008c83778
Jerry D'Antonio <stumpjumper@gmail.com>
2016-04-10 17:22:58 -0400

    Curry FTW, yo.

--- yahtzee/jdantonio/lib/yahtzee.js
+++ yahtzee/jdantonio/lib/yahtzee.js
@@ -1,4 +1,10 @@
-/*eslint no-console: ["error", { allow: ["log"] }] */
+var rules = new Map();
+rules.set('ones', (dice) => { return sumForNumberRule(1, dice); });
+rules.set('twos', (dice) => { return sumForNumberRule(2, dice); });
+rules.set('threes', (dice) => { return sumForNumberRule(3, dice); });
+rules.set('fours', (dice) => { return sumForNumberRule(4, dice); });
+rules.set('fives', (dice) => { return sumForNumberRule(5, dice); });
+rules.set('sixes', (dice) => { return sumForNumberRule(6, dice); });
 
 function sumForNumberRule(number, dice) {
   return dice.reduce(function(previousValue, currentValue/*, currentIndex, array*/) {
@@ -11,26 +17,10 @@ function sumForNumberRule(number, dice) {
 
 module.exports.score = function(rule, dice) {
   var score = -1;
+  var func = rules.get(rule.toLowerCase());
 
-  switch (rule.toLowerCase()) {
-  case 'ones':
-    score = sumForNumberRule(1, dice);
-    break;
-  case 'twos':
-    score = sumForNumberRule(2, dice);
-    break;
-  case 'threes':
-    score = sumForNumberRule(3, dice);
-    break;
-  case 'fours':
-    score = sumForNumberRule(4, dice);
-    break;
-  case 'fives':
-    score = sumForNumberRule(5, dice);
-    break;
-  case 'sixes':
-    score = sumForNumberRule(6, dice);
-    break;
+  if (func) {
+    score = func(dice);
   }
 
   return score;
