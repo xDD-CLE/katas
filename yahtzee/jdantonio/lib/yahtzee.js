9023d796d6f7a5da09417474a4a62b1c329a940e
Jerry D'Antonio <stumpjumper@gmail.com>
2016-04-10 16:51:04 -0400

    Simplest implementation of threes, fours, fives, and sixes.

--- yahtzee/jdantonio/lib/yahtzee.js
+++ yahtzee/jdantonio/lib/yahtzee.js
@@ -10,7 +10,7 @@ function sumForNumberRule(number, dice) {
 }
 
 module.exports.score = function(rule, dice) {
-  var score = 0;
+  var score = -1;
 
   switch (rule.toLowerCase()) {
   case 'ones':
@@ -18,6 +18,18 @@ module.exports.score = function(rule, dice) {
     break;
   case 'twos':
     score = sumForNumberRule(2, dice);
+    break;
+  case 'threes':
+    score = sumForNumberRule(3, dice);
+    break;
+  case 'fours':
+    score = sumForNumberRule(4, dice);
+    break;
+  case 'fives':
+    score = sumForNumberRule(5, dice);
+    break;
+  case 'sixes':
+    score = sumForNumberRule(6, dice);
     break;
   }
 
--- yahtzee/jdantonio/test/yahtzee_test.js
+++ yahtzee/jdantonio/test/yahtzee_test.js
@@ -2,21 +2,21 @@ var yahtzee = require('../lib/yahtzee.js');
 
 exports.testOnesRuleWithZeroOnes = function(test) {
   var expected = 0;
-  var actual = yahtzee.score('ones', [2, 3, 4, 5, 6]);
+  var actual = yahtzee.score('Ones', [2, 3, 4, 5, 6]);
   test.equal(expected, actual);
   test.done();
 };
 
 exports.testOnesRuleWithSomeOnes = function(test) {
   var expected = 3;
-  var actual = yahtzee.score('ones', [1, 2, 1, 4, 1]);
+  var actual = yahtzee.score('Ones', [1, 2, 1, 4, 1]);
   test.equal(expected, actual);
   test.done();
 };
 
 exports.testOnesRuleWithAllOnes = function(test) {
   var expected = 5;
-  var actual = yahtzee.score('ones', [1, 1, 1, 1, 1]);
+  var actual = yahtzee.score('Ones', [1, 1, 1, 1, 1]);
   test.equal(expected, actual);
   test.done();
 };
@@ -38,6 +38,90 @@ exports.testTwosRuleWithSomeTwos = function(test) {
 exports.testTwosRuleWithAllTwos = function(test) {
   var expected = 10;
   var actual = yahtzee.score('Twos', [2, 2, 2, 2, 2]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testThreesRuleWithZeroThrees = function(test) {
+  var expected = 0;
+  var actual = yahtzee.score('Threes', [1, 2, 4, 5, 6]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testThreesRuleWithSomeThrees = function(test) {
+  var expected = 6;
+  var actual = yahtzee.score('Threes', [1, 3, 1, 3, 1]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testThreesRuleWithAllThrees = function(test) {
+  var expected = 15;
+  var actual = yahtzee.score('Threes', [3, 3, 3, 3, 3]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testFoursRuleWithZeroFours = function(test) {
+  var expected = 0;
+  var actual = yahtzee.score('Fours', [1, 2, 3, 5, 6]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testFoursRuleWithSomeFours = function(test) {
+  var expected = 12;
+  var actual = yahtzee.score('Fours', [4, 1, 4, 1, 4]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testFoursRuleWithAllFours = function(test) {
+  var expected = 20;
+  var actual = yahtzee.score('Fours', [4, 4, 4, 4, 4]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testFivesRuleWithZeroFives = function(test) {
+  var expected = 0;
+  var actual = yahtzee.score('Fives', [1, 2, 3, 4, 6]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testFivesRuleWithSomeFives = function(test) {
+  var expected = 10;
+  var actual = yahtzee.score('Fives', [1, 1, 1, 5, 5]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testFivesRuleWithAllFives = function(test) {
+  var expected = 25;
+  var actual = yahtzee.score('Fives', [5, 5, 5, 5, 5]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testSixesRuleWithZeroSixes = function(test) {
+  var expected = 0;
+  var actual = yahtzee.score('Sixes', [1, 2, 3, 4, 5]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testSixesRuleWithSomeSixes = function(test) {
+  var expected = 24;
+  var actual = yahtzee.score('Sixes', [1, 6, 6, 6, 6]);
+  test.equal(expected, actual);
+  test.done();
+};
+
+exports.testSixesRuleWithAllSixes = function(test) {
+  var expected = 30;
+  var actual = yahtzee.score('Sixes', [6, 6, 6, 6, 6]);
   test.equal(expected, actual);
   test.done();
 };
