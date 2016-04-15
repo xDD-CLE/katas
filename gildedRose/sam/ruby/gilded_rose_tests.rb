7d1f78e07565c31962586f257f891610235992bc
Sam Jones <sam@samljones.com>
2016-04-15 12:24:43 -0400

    wrapping gilded rose in tests

---
+++ gildedRose/sam/ruby/gilded_rose_tests.rb
@@ -0,0 +1,73 @@
+require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'test/unit'
+
+class TestUntitled < Test::Unit::TestCase
+
+  def item_tester(item:, new_sell_in:, new_quality:)
+    items = [item]
+    GildedRose.new(items).update_quality()
+    assert_equal(new_sell_in, items.first.sell_in)
+    assert_equal(new_quality, items.first.quality)
+  end
+
+  def test_foo
+    item = Item.new(name: "foo", sell_in: 5, quality: 10)
+    item_tester(item: item, new_sell_in: 4, new_quality: 9)
+  end
+
+  def test_foo_0_days
+    item = Item.new(name: "foo", sell_in: 0, quality: 10)
+    item_tester(item: item, new_sell_in: -1, new_quality: 8)
+  end
+
+  def test_brie
+    item = Item.new(name: "Aged Brie", sell_in: 5, quality: 10)
+    item_tester(item: item, new_sell_in: 4, new_quality: 11)
+  end
+
+  def test_brie_0_days
+    item = Item.new(name: "Aged Brie", sell_in: 0, quality: 10)
+    item_tester(item: item, new_sell_in: -1, new_quality: 12)
+  end
+
+  def test_brie_0_days_high_quality
+    item = Item.new(name: "Aged Brie", sell_in: 0, quality: 50)
+    item_tester(item: item, new_sell_in: -1, new_quality: 50)
+  end
+
+  def test_concert_11_days
+    item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 11, quality: 10)
+    item_tester(item: item, new_sell_in: 10, new_quality: 11)
+  end
+
+  def test_concert_10_days
+    item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 10)
+    item_tester(item: item, new_sell_in: 9, new_quality: 12)
+  end
+
+  def test_concert_6_days
+    item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 6, quality: 10)
+    item_tester(item: item, new_sell_in: 5, new_quality: 12)
+  end
+
+  def test_concert_5_days
+    item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 10)
+    item_tester(item: item, new_sell_in: 4, new_quality: 13)
+  end
+
+  def test_concert_0_days
+    item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 10)
+    item_tester(item: item, new_sell_in: -1, new_quality: 0)
+  end
+
+  def test_concert_high_quality
+    item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 50)
+    item_tester(item: item, new_sell_in: 9, new_quality: 50)
+  end
+
+  def test_sulfuras
+    item = Item.new(name: "Sulfuras, Hand of Ragnaros", sell_in: 10, quality: 50)
+    item_tester(item: item, new_sell_in: 10, new_quality: 50)
+  end
+
+end
