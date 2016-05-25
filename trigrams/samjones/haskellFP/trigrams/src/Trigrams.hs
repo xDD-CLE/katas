9ecafc0cafb257d5723e5ff24dae24cea9390b29
Sam Jones <sam@samljones.com>
2016-05-25 13:00:25 -0400

    TDDing lower levels

--- trigrams/samjones/haskellFP/trigrams/src/Trigrams.hs
+++ trigrams/samjones/haskellFP/trigrams/src/Trigrams.hs
@@ -1,16 +1,24 @@
-module Trigrams (trigrams) where
+module Trigrams (trigrams,
+               consecutiveByThree,
+               createTrigram) where
 
 import qualified Data.Map as Map
 
 type Trigrams = Map.Map String [String]
 
 trigrams :: String -> Trigrams
-trigrams = foldl insertVal Map.empty . fmap createTrigram . splitWordsIntoThree
+trigrams = foldl insertVal Map.empty . fmap createTrigram . consecutiveByThree
   where
-    insertVal acc (k, v) = Map.insertWith (++) k v acc 
+    insertVal acc (k, v) = Map.insertWith prepend' k v acc 
+    prepend' = flip (++)
 
-splitWordsIntoThree :: String -> [[String]]
-splitWordsIntoThree = undefined
+consecutiveByThree :: String -> [[String]]
+consecutiveByThree = takeWhile lenghIsThree  . consecutiveByThree' . words
+  where
+    consecutiveByThree' [] = [[]]
+    consecutiveByThree' xs@(_:rest) = take 3 xs : consecutiveByThree' rest
+    lenghIsThree = (==3) . length
 
 createTrigram :: [String] -> (String, [String])
-createTrigram = undefined
+createTrigram [one, two, three] = (unwords [one, two], [three])
+createTrigram _ = error "Trigram can only be created from a list of three words"
--- trigrams/samjones/haskellFP/trigrams/test/TrigramSpec.hs
+++ trigrams/samjones/haskellFP/trigrams/test/TrigramSpec.hs
@@ -5,7 +5,9 @@ import Test.Hspec
 import qualified Data.Map as Map
 
 
-import Trigrams
+import Trigrams (trigrams,
+                 consecutiveByThree,
+                 createTrigram)
 
 main :: IO ()
 main = hspec spec
@@ -18,3 +20,14 @@ spec = do
                                                         ("wish I", ["may", "might"]),
                                                         ("may I", ["wish"]),
                                                         ("I may", ["I"])]
+
+  describe "Splitting words" $ do
+    it "should get consecutive words by three" $ do
+      consecutiveByThree "foo bar baz hello" `shouldBe` [["foo", "bar", "baz"], ["bar", "baz", "hello"]]
+
+    it "should get consecutive words by three when less than 3 words" $ do
+      consecutiveByThree "foo bar" `shouldBe` []
+
+  describe "Creating a Trigram" $ do
+    it "should create trigram from a list of 3 words by making the first two words the key and the last the value" $ do
+      createTrigram ["wish", "I", "may"] `shouldBe` ("wish I", ["may"])
